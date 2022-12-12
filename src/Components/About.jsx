7c2c489887cf624931ee3c1e225b28775e2e16d9
Anthony_256 <101401469+AnthonyByansi@users.noreply.github.com>
2022-12-12 11:18:04 +0000

    Update the cover image Update the cards for the project title

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,8 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
-
+  "Hi there! My name is Anthony Byansi, and I am a UX/UI designer with a passion for creating intuitive and engaging digital experience. I have a background in design, with a deep understanding of color theory, typography, layout, and user-centered design principles. I also have experience with design tools such as Sketch, Figma, and Adobe Creative Suite. As a UX/UI designer.  "
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "As a UX and UI designer, I'm passionate about creating intuitive and user-friendly designs that enhance the user experience. my career goal is to improve the way people interact with technology by designing easy-to-use interfaces. I'm constantly exploring new design techniques and staying up-to-date with the latest design trends.";
+  "As a UX and UI designer, I'm passionate about creating intuitive and user-friendly designs that enhance the user experience. my career goal is to improve the way people interact with technology by designing easy-to-use interfaces. I'm constantly exploring new design techniques and staying up-to-date with the latest design trends. I am responsible for designing the user experience and user interface of digital products, such as websites and mobile apps. I create wireframes, prototypes, and high-fidelity designs, as well as conduct user research and testing to ensure that the product is intuitive, user-friendly, and visually appealing. My goal as a UX/UI designer is to create designs that meet the needs of users and improve their overall experience with the product. I believe that great design has the power to drive user engagement, conversion, and satisfaction, and I am committed to helping businesses and organizations achieve their goals through thoughtful and effective design. If you have a project that you think would benefit from my UX/UI design skills, please don't hesitate to get in touch. I would love to discuss your goals and ideas, and help you bring your vision to life. Thank you for considering me for your design needs!";
 
 const About = () => {
   return (
